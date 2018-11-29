2ab520252dbb5cc4eba6e8f2c897405d51e8af43
mariette souppe <mariette@eduroam-169-233-180-184.ucsc.edu>
2018-11-29 12:53:10 -0800

    Camera calibration complete

--- calibration.py
+++ calibration.py
@@ -1,14 +1,11 @@
 import os
 import cv2 as cv
-import matplotlib.pyplot as plt
 import numpy as np
+import glob
 
-# My library
-import util as ut
-
-def camera_calibration(imgs):
+def camera_calibration():
 	# termination criteria
-	criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+	criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 	# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
 	objp = np.zeros((6*7,3), np.float32)
@@ -18,35 +15,42 @@ def camera_calibration(imgs):
 	objpoints = [] # 3d point in real world space
 	imgpoints = [] # 2d points in image plane.
 
-	images = ut.load_images(imgs)
+	images = glob.glob('images/chessboard/*.jpg')
+
+	gray = None
 
 	for fname in images:
+		img = cv.imread(fname)
+		gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+		
 		# Find the chess board corners
-		ret, corners = cv2.findChessboardCorners(gray, (7,6),None)
+		ret, corners = cv.findChessboardCorners(gray, (7,6),None)
 
 		# If found, add object points, image points (after refining them)
 		if ret == True:
 			objpoints.append(objp)
 
-			cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+			cv.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
 			imgpoints.append(corners)
 
 			# Draw and display the corners
-			cv2.drawChessboardCorners(img, (7,6), corners2,ret)
-			cv2.imshow('img',img)
-			cv2.waitKey(500)
+			cv.drawChessboardCorners(img, (7,6), corners, ret)
+			cv.imshow('img',img)
+			cv.waitKey(500)
 
-	ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
-	return [ret, mtx, dist, rvecs, tvecs]
+	cv.destroyAllWindows()
+
+	ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+	mse = mean_sqaure_error(imgpoints, objpoints, rvecs, tvecs, mtx, dist)
+
+	return ret, mtx, dist, rvecs, tvecs, mse
 
 
-def mean_sqaure_error(objpoints):
+def mean_sqaure_error(imgpoints, objpoints, rvecs, tvecs, mtx, dist):
 	mean_error = 0
-	for i in xrange(len(objpoints)):
-	    imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
-	    error = cv2.norm(imgpoints[i],imgpoints2, cv2.NORM_L2)/len(imgpoints2)
-	    tot_error += error
+	for i in range(len(objpoints)):
+	    imgpoints2, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+	    error = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2)/len(imgpoints2)
+	    mean_error += error
 
-	print "total error: ", mean_error/len(objpoints)
-
-	return mean_error/len(objpoints
+	return mean_error/len(objpoints)
--- main.py
+++ main.py
@@ -2,26 +2,34 @@ import os
 import cv2 as cv
 import matplotlib.pyplot as plt
 import numpy as np
+np.set_printoptions(suppress=True, formatter={'float_kind':'{:0.5f}'.format})
 
 # My library
-import util as ut
+import calibration as cal
+import camera_pose as cam
+import plane_sweeping as pl
 
 # Get current working directory and locate images
 curr_work_dir = os.getcwd()
 img_path = curr_work_dir + "/images/"
-chessboard_path = img_path + "/chessboard/"
-scene_path = img_path + "/scene/"
+chessboard_path = img_path + "chessboard/"
+scene_path = img_path + "scene/"
 
 #-----------------------------------------------------------------#
 #                              Part 1 
 #-----------------------------------------------------------------#
 # Chessboard pattern images
+#chessboard_imgs = ut.load_images(chessboard_path)
 
 # Intrinsic matrix, K
+ret, mtx, dist, rvecs, tvecs, mse = cal.camera_calibration()
+print("K:\n", mtx)
 
 # Radial distortion coefficients
+print("\nRadial distortion coefficients:\n", dist)
 
 # Reprojection mean square error
+print("\nMean square error: ", "{:.5f}".format(mse))
 
 #-----------------------------------------------------------------#
 #                              Part 2 
